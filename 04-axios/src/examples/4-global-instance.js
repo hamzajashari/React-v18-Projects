649e77b87081169f0ad062b4fab6c111a55d256b
Hamza Jashari <hamza.jashari@evn.mk>
2024-03-19 16:43:40 +0100

    axios intro

---
+++ 04-axios/src/examples/4-global-instance.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+const productsUrl = "https://course-api.com/react-store-products";
+const randomUserUrl = "https://randomuser.me/api";
+
+const GlobalInstance = () => {
+  const fetchData = async () => {
+    try {
+      const response1 = await axios(productsUrl);
+      
+      const response2 = await axios(randomUserUrl);
+      console.log(response1.data);
+      
+      console.log(response2.data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <h2 className="text-center">global instance</h2>;
+};
+export default GlobalInstance;
